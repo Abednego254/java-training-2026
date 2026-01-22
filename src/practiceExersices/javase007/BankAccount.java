559d54097d2fdba725932ae5b2bca63fb9b967ea
Abednego254 <kaumeabednego3813@gmail.com>
2026-01-22 21:49:02 +0300

    OOP Practice Exersice 1

---
+++ src/practiceExersices/javase007/BankAccount.java
@@ -0,0 +1,49 @@
+package practiceExersices.javase007;
+
+public class BankAccount {
+    public String accountNumber;
+    public String accountHolder;
+    public double balance;
+
+    public BankAccount () {
+        accountHolder = null;
+        accountNumber = null;
+        balance = 0.0;
+    }
+
+    public BankAccount (String number, String holder) {
+        this.accountNumber = number;
+        this.accountHolder = holder;
+    }
+
+    public BankAccount (String number, String holder, double balance) {
+        this.accountNumber = number;
+        this.accountHolder = holder;
+        this.balance = balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+        System.out.println("You have deposited "+ amount +". Your balance is "+ balance);
+    }
+
+    public void withdraw(int amount) {
+        balance  -= amount;
+        System.out.println("You have withdrawn "+ amount +". Your balance is "+ balance);
+    }
+
+    public void displayBalance () {
+        System.out.println("Holder: "+accountHolder);
+        System.out.println("Number: "+accountNumber);
+        System.out.println("Balance: "+ balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.accountHolder = "Abednego";
+        bankAccount.accountNumber = "A0123";
+        bankAccount.balance = 10000.0;
+        bankAccount.displayBalance();
+    }
+
+}
