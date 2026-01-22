92b95caa210ddc71f11353420b66bd2d71debefd
Abednego254 <kaumeabednego3813@gmail.com>
2026-01-22 23:43:08 +0300

    Student Management System Practice Exersice 3

---
+++ src/practiceExersices/javase007/Student.java
@@ -0,0 +1,54 @@
+package practiceExersices.javase007;
+
+import java.util.Arrays;
+
+public class Student {
+    public String name;
+    public int id;
+    public double GPA;
+    public int []grades;
+    //Default constructor
+    public Student() {
+        grades = new int[5];
+    }
+
+    public Student(String studentName, int studentId) {
+        this.name = studentName;
+        this.id = studentId;
+        grades = new int[5];
+    }
+
+    public Student(String studentName, int studentId, int[] studentGrades) {
+        this.name = studentName;
+        this.id = studentId;
+        this.grades = grades;
+    }
+
+    public Student(String studentName, int studentId, int[] studentGrades, double studentGpa) {
+        this.name = studentName;
+        this.id = studentId;
+        this.grades = studentGrades;
+        this.GPA = studentGpa;
+    }
+
+    public void addGrade(int index, int grade) {
+        grades[index] = grade;
+    }
+
+    public double calculateGPA() {
+        int totalMarks = 0;
+        for (int i = 0; i < grades.length; i++) {
+            totalMarks += grades[i];
+        }
+        GPA = (double) totalMarks / grades.length;
+        return GPA;
+    }
+
+    public void printStudentInfor() {
+        System.out.println("Name: "+ name);
+        System.out.println("ID: "+ id);
+        System.out.println("Grades: "+ Arrays.toString(grades));
+        System.out.println("GPA: "+ calculateGPA());
+        System.out.println("-------------------------");
+    }
+}
