198c26d304549a47c4380957c452a8799d869053
Abednego254 <kaumeabednego3813@gmail.com>
2026-01-26 18:21:42 +0300

    OOP Mdogo mdogo

---
+++ src/practiceExersices/week2/javase007OOP/StudentManager.java
@@ -0,0 +1,33 @@
+package practiceExersices.week2.javase006.javase007OOP;
+
+public class StudentManager {
+    public static void main(String[] args) {
+        Student[] student = new Student[3];
+
+        student[0] = new Student("ABEDNEGO", 001);
+        student[1] = new Student("KAUME", 002);
+        student[2] = new Student("MONICAH", 003);
+
+        student[0].addGrade(0, 85);
+        student[0].addGrade(1, 92);
+        student[0].addGrade(2, 78);
+        student[0].addGrade(3, 90);
+        student[0].addGrade(4, 88);
+
+        student[1].addGrade(0, 75);
+        student[1].addGrade(1, 80);
+        student[1].addGrade(2, 68);
+        student[1].addGrade(3, 72);
+        student[1].addGrade(4, 75);
+
+        student[2].addGrade(0, 90);
+        student[2].addGrade(1, 95);
+        student[2].addGrade(2, 88);
+        student[2].addGrade(3, 92);
+        student[2].addGrade(4, 94);
+
+        for (int i = 0; i <student.length; i++) {
+            student[i].printStudentInfor();
+        }
+    }
+}
