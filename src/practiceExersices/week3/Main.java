1ef36af0a83f5a48eeeae5daac0490b18746fa65
Abednego254 <kaumeabednego3813@gmail.com>
2026-01-26 18:23:44 +0300

    Polymorphism here

---
+++ src/practiceExersices/week3/Main.java
@@ -0,0 +1,7 @@
+//package practiceExersices.week3;
+//
+//public class Main {
+//    public int add(int... numbers) {
+//
+//    }
+//}
---
+++ src/practiceExersices/week3/Vehicle.java
@@ -0,0 +1,7 @@
+package practiceExersices.week3;
+
+public class Vehicle {
+    public void startEngine() {
+        System.out.println("Press to start");
+    }
+}
