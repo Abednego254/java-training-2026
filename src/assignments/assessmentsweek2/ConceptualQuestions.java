0b3e0f07bbf488f21a993e6c9f3d7cb4d8a5697c
Abednego254 <kaumeabednego3813@gmail.com>
2026-01-26 18:51:27 +0300

    Renamed file

--- src/assignments/assessmentsweek2/ConceptualQuestions.java
+++ src/assignments/assessmentsweek2/ConceptualQuestions.java
@@ -1,3 +1,5 @@
+package assignments.assessmentsweek2;
+
 public class ConceptualQuestions {
     /*
         ===============================================================
--- src/practiceExersices/week3/Main.java
+++
@@ -1,7 +0,0 @@
-//package practiceExersices.week3;
-//
-//public class Main {
-//    public int add(int... numbers) {
-//
-//    }
-//}
